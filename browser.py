cd3a1b8bdb50c089c8e47d681e80a9744f106177
gabriel.cartelli <gabriel.cartelli@cwi.com.br>
2020-01-20 11:19:41 -0300

    projeto inicial

--- browser.py
+++ browser.py
@@ -2,13 +2,18 @@ from selenium import webdriver
 
 
 class Browser(object):
+    # Inicia o browser chrome, mas pode ser feito com outros como Firefox, Safari e IE
     driver = webdriver.Chrome()
+    # Define o tempo máximo para carregamento da página
     driver.set_page_load_timeout(30)
+    # Maximiza a janela do browser ao ser iniciado
     driver.maximize_window()
 
+    # Fecha o browser
     def browser_quit(self):
         self.driver.quit()
 
+    # Limpa o browser
     def browser_clear(self):
         self.driver.delete_all_cookies()
         self.driver.execute_script('window.localStorage.clear()')
